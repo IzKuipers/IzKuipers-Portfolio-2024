45928414132c86e3fdc056590180195ad40d8b14
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-02-29 11:00:24 +0100

    Fix mobile issues

--- src/lib/Right/Welcome.svelte
+++ src/lib/Right/Welcome.svelte
@@ -5,7 +5,7 @@
 <div class="welcome">
   <img src={Me} alt="" />
   <div class="content">
-    <h1>Welcome!</h1>
+    <h1>Hello!</h1>
     <p class="subtitle">My name is Izaak.</p>
   </div>
 </div>
