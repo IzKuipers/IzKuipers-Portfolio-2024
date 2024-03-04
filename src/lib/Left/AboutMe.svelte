3b095ec1405335065ed8149a1203ef8273da79bf
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-03-04 10:08:41 +0100

    Printers are a disgrace

--- src/lib/Left/AboutMe.svelte
+++ src/lib/Left/AboutMe.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <p class="about-me">
-  I'm a 16-year old passionate full-stack developer from the Netherlands. I aim
+  I'm a 17-year old passionate full-stack developer from the Netherlands. I aim
   to write software that's easy to use and that just gets the job done. In 2020
   I started my biggest project yet; <ArcOSLink />.
+  <br /><br />
+  And remember... "Printers are a disgrace"!
 </p>
